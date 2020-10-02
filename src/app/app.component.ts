f204cffe4c564770f04ff40feb8927d2e8956a30
Ivan Hrushevich <ivan.grushevich1@gmail.com>
2020-10-03 02:37:07 +0300

    create basic action

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+
+import { login } from './../store/actions';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private readonly store: Store) {}
+  public onLoginButtonClick(): void {
+    this.store.dispatch(login({ username: 'username', password: 'password' }));
+  }
+}
