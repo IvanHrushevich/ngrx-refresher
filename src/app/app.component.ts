4b1af1ab21cd48138855eefe2657a69b03ab4d76
Ivan Hrushevich <ivan.grushevich1@gmail.com>
2020-10-04 13:17:51 +0300

    add away score

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Observable } from 'rxjs';
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 
-import { homeScore } from '../store/actions';
-import { selectHome } from '../store/selectors';
+import { awayScore, homeScore } from '../store/actions';
+import { selectAway, selectHome } from '../store/selectors';
 
 @Component({
   selector: 'app-root',
@@ -13,15 +13,21 @@ import { selectHome } from '../store/selectors';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent implements OnInit {
-  public home$: Observable<any>;
+  public home$: Observable<number>;
+  public away$: Observable<number>;
 
   constructor(private readonly store: Store) {}
 
   public ngOnInit(): void {
     this.home$ = this.store.select(selectHome, { numberToAdd: 100 });
+    this.away$ = this.store.select(selectAway, { numberToAdd: 100 });
   }
 
   public onHomeScoreButtonClick(): void {
     this.store.dispatch(homeScore());
+  }
+
+  public onAwayScoreButtonClick(): void {
+    this.store.dispatch(awayScore());
   }
 }
--- src/store/selectors/scoreboard.selecors.ts
+++ src/store/selectors/scoreboard.selecors.ts
@@ -1,10 +1,18 @@
-import { createSelector } from '@ngrx/store';
+import { createFeatureSelector, createSelector } from '@ngrx/store';
 
 import { AppState, GameState } from '../models';
+import { scoreboardFeatureKey } from '../reducers';
 
-export const selectGame = (state: AppState) => state.game;
+export const selectGame = createFeatureSelector<AppState, GameState>(
+  scoreboardFeatureKey
+);
 
 export const selectHome = createSelector(
   selectGame,
   (game: GameState, props: any) => game.home + props.numberToAdd
+);
+
+export const selectAway = createSelector(
+  selectGame,
+  (game: GameState, props: any) => game.away + props.numberToAdd
 );
